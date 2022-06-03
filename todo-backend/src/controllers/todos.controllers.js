f430050e9acf113d8c71f315b83312312f2a541e
Senesh Deshan <senesh@hsenidoutsourcing.com>
2022-06-03 08:45:43 +0530

    Added API Documentation

--- todo-backend/src/controllers/todos.controllers.js
+++ todo-backend/src/controllers/todos.controllers.js
@@ -4,6 +4,83 @@ import mongoose from 'mongoose'
 import Todo from '../models/todo.js';
 import env from '../../config.js'
 
+/**
+ * @apiDefine TodoSuccessResponse
+ *
+ * @apiSuccess {String} _id ID of the Todo.
+ * @apiSuccess {String} text Title of the Todo.
+ * @apiSuccess {String} description Description of the Todo.
+ * @apiSuccess {Boolean} done Completed state of the Todo.
+ * @apiSuccess {String} user User Id of the Todo.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "_id": "62962e0fe6537ae9a6d39dbf",
+ *         "text": "Bring Milk",
+ *         "description": "Newland Brand Milk x 2",
+ *         "done": false,
+ *         "user": "6294ff992e9437d705b67ac6"
+ *     }
+ */
+
+/**
+ * @apiDefine UserNotFoundError
+ *
+ * @apiError UserNotFound The requested User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Found
+ *     {
+ *       "error": "Unauthorized"
+ *     }
+ */
+
+/**
+ * @apiDefine RequestAuthHeader
+ *
+ * @apiHeader (jwt token){String} Authorization  jwt authorization
+ * @apiHeaderExample {json} Request-Header-Example:
+ * {
+ *  "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbl9uYW1lIjoiaHVudGVyIiwiaWF0IjoxNTg5NzgyNjYyLCJleHAiOjE1ODk5NTU0NjJ9.ART87U90VZTkuuJznifGJWhbGSsv9eW7kkUaRopXfgE"  
+ * }
+ */
+
+/**
+ * @api {get} /todo Request all Todos of the User
+ * @apiName Get All Todos
+ * @apiGroup Todo
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess {String} _id ID of the Todo.
+ * @apiSuccess {String} text Title of the Todo.
+ * @apiSuccess {String} description Description of the Todo.
+ * @apiSuccess {Boolean} done Completed state of the Todo.
+ * @apiSuccess {String} user User Id of the Todo.
+ *
+ * @apiUse RequestAuthHeader
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         {
+ *             "_id": "6295d1f0fdb025d18fceb865",
+ *             "text": "Pay Bills",
+ *             "description": "Telephone\nElectricity",
+ *             "done": false,
+ *             "user": "6294ff992e9437d705b67ac6"
+ *         },
+ *         {
+ *             "_id": "62962e0fe6537ae9a6d39dbf",
+ *             "text": "Bring Milk",
+ *             "description": "Newland Brand Milk x 2",
+ *             "done": true,
+ *             "user": "6294ff992e9437d705b67ac6",
+ *         }
+ *     ]
+ * 
+ * @apiUse UserNotFoundError
+ */
 const getTodos = (req, res) => {
     if (req.cookies && req.cookies.token) {
         const payload = jwt.verify(req.cookies.token, env.secret);
@@ -16,6 +93,20 @@ const getTodos = (req, res) => {
     }
 }
 
+/**
+ * @api {put} /todo Create a Todo of the User
+ * @apiName Create New Todo
+ * @apiGroup Todo
+ * @apiVersion 1.0.0
+ *
+ * @apiUse RequestAuthHeader
+ * 
+ * @apiBody {String} text Title of the Todo.
+ * @apiBody {String} description Description of the Todo.
+ * 
+ * @apiUse UserSuccessResponse
+ * @apiUse UserNotFoundError
+ */
 const addTodo = (req, res) => {
     if (req.cookies && req.cookies.token) {
         const payload = jwt.verify(req.cookies.token, env.secret);
@@ -36,6 +127,21 @@ const addTodo = (req, res) => {
     }
 }
 
+/**
+ * @api {post} /todo Update a Todo of the User
+ * @apiName Update Existing Todo
+ * @apiGroup Todo
+ * @apiVersion 1.0.0
+ *
+ * @apiUse RequestAuthHeader
+ * 
+ * @apiBody {String} [text] Title of the Todo.
+ * @apiBody {String} [description] Description of the Todo.
+ * @apiBody {Boolean} [done] Completed state of the Todo.
+ * 
+ * @apiUse UserSuccessResponse
+ * @apiUse UserNotFoundError
+ */
 const updateTodo = (req, res) => {
     if (req.cookies && req.cookies.token) {
         const payload = jwt.verify(req.cookies.token, env.secret);
@@ -56,6 +162,24 @@ const updateTodo = (req, res) => {
     }
 }
 
+/**
+ * @api {delete} /todo Delete a Todo of the User
+ * @apiName Delete Existing Todo
+ * @apiGroup Todo
+ * @apiVersion 1.0.0
+ *
+ * @apiUse RequestAuthHeader
+ * 
+ * @apiSuccess {Boolean} success Todo delete status.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "success": true
+ *     }
+ * 
+ * @apiUse UserNotFoundError
+ */
 const deleteTodo = (req, res) => {
     if (req.cookies && req.cookies.token) {
         const payload = jwt.verify(req.cookies.token, env.secret);
--- todo-backend/src/controllers/users.controllers.js
+++ todo-backend/src/controllers/users.controllers.js
@@ -4,6 +4,48 @@ import bcrypt from 'bcrypt'
 import User from '../models/user.js';
 import env from '../../config.js'
 
+/**
+ * @apiDefine UserSuccessResponse
+ *
+ * @apiSuccess {String} id ID of the User.
+ * @apiSuccess {String} email Email of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "id": "6294ff992e9437d705b67ac6",
+ *         "email": "hello@example.com"
+ *     }
+ */
+
+
+/**
+ * @apiDefine UserNotFoundError
+ *
+ * @apiError UserNotFound The requested User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Found
+ *     {
+ *       "error": "Unauthorized"
+ *     }
+ */
+
+/**
+ * @api {get} /user/details Request User information
+ * @apiName Get User Details
+ * @apiGroup User
+ * @apiVersion 1.0.0
+ *
+ * @apiHeader (jwt token){String} Authorization  jwt authorization
+ * @apiHeaderExample {json} Request-Header-Example:
+ * {
+ *  "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbl9uYW1lIjoiaHVudGVyIiwiaWF0IjoxNTg5NzgyNjYyLCJleHAiOjE1ODk5NTU0NjJ9.ART87U90VZTkuuJznifGJWhbGSsv9eW7kkUaRopXfgE"  
+ * }
+ * 
+ * @apiUse UserSuccessResponse
+ * @apiUse UserNotFoundError
+ */
 const userDetails = (req, res) => {
     if (req.cookies && req.cookies.token) {
         const payload = jwt.verify(req.cookies.token, env.secret);
@@ -19,6 +61,25 @@ const userDetails = (req, res) => {
     }
 }
 
+/**
+ * @api {post} /user/register Register a new User
+ * @apiName User Register
+ * @apiGroup User
+ * @apiVersion 1.0.0
+ *
+ * @apiBody {String} email Email of the User.
+ * @apiBody {String} password Password of the User.
+ *
+ * @apiUse UserSuccessResponse
+ * 
+ * @apiError UserAlreadyExists A User already exists with given email.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 409 Conflict
+ *     {
+ *       "error": "User already exists"
+ *     }
+ */
 const userRegister = (req, res) => {
     const { email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -42,6 +103,18 @@ const userRegister = (req, res) => {
     })
 }
 
+/**
+ * @api {post} /user/login Login an existing User
+ * @apiName User Login
+ * @apiGroup User
+ * @apiVersion 1.0.0
+ *
+ * @apiBody {String} email Email of the User.
+ * @apiBody {String} password Password of the User.
+ *
+ * @apiUse UserSuccessResponse
+ * @apiUse UserNotFoundError
+ */
 const userLogin = (req, res) => {
     const { email, password } = req.body;
 
@@ -69,6 +142,20 @@ const userLogin = (req, res) => {
 
 }
 
+/**
+ * @api {post} /user/logout Logout an User
+ * @apiName User Logout
+ * @apiGroup User
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess {Boolean} success logout status.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "success": true
+ *     }
+ */
 const userLogout = (req, res) => {
     res.cookie('token', '').json({ "success": true });
 }
